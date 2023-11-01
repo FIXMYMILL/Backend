e08ff97d3ffcc32edac22b9911b58f066deda3a3
sahil <sahillathib@gmail.com>
2023-11-01 17:56:02 +0530

    roll back

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const bodyParser = require("body-parser");
 
 require("dotenv").config();
 app.use(cors());
-app.use(bodyParser.json());
+app.use(express.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 
